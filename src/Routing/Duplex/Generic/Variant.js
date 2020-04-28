471a45fe9fb82a9ddd3bfbb8ecd196a88e6ad622
Tomasz Rybarczyk <paluho@gmail.com>
2020-04-28 20:44:45 +0200

    Add `copyRecord` FFI

---
+++ src/Routing/Duplex/Generic/Variant.js
@@ -0,0 +1,14 @@
+"use strict";
+
+exports.copyRecord = function(rec) {
+  var copy = {};
+  for (var key in rec) {
+    if ({}.hasOwnProperty.call(rec, key)) {
+      copy[key] = rec[key];
+    }
+  }
+  return copy;
+};
+
+
+
