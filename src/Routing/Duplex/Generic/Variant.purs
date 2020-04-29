fcae3d9d0bd09f72fa97fcb7c1d1e0c38c8f703a
Tomasz Rybarczyk <paluho@gmail.com>
2020-04-29 07:00:39 +0200

    Newtype Record.Builder as record Updater

--- src/Routing/Duplex/Generic/Variant.purs
+++ src/Routing/Duplex/Generic/Variant.purs
@@ -9,11 +9,13 @@ import Prim.Row (class Cons) as Row
 import Prim.RowList (Cons, Nil) as RowList
 import Prim.RowList (class RowToList, kind RowList)
 import Record (get) as Record
+import Record.Builder (build, Builder) as Record.Builder
 import Record.Unsafe (unsafeSet)
 import Routing.Duplex (RouteDuplex(..), RouteDuplex', prefix)
 import Routing.Duplex.Parser (RouteParser)
 import Routing.Duplex.Printer (RoutePrinter)
 import Type.Prelude (RLProxy(..), SProxy(..), reflectSymbol)
+import Unsafe.Coerce (unsafeCoerce)
 
 prs ∷ ∀ a. RouteDuplex' a → RouteParser a
 prs (RouteDuplex _ p) = p
@@ -79,24 +81,28 @@ variant routes = RouteDuplex printer parser
     printer = variantPrinter (RLProxy ∷ RLProxy rl) routes
     parser = variantParser (RLProxy ∷ RLProxy rl) routes
 
--- | Rip from purescript-record
-foreign import copyRecord ∷ ∀ r1. Record r1 → Record r1
-
 -- | In place record updater
-data Updater r = Updater (r → r)
+newtype Updater r = Updater (Record.Builder.Builder r r)
 
 instance semigroupUpdater ∷ Semigroup (Updater r) where
-  append (Updater r1) (Updater r2) = Updater (r1 <<< r2)
+  append (Updater b1) (Updater b2) = Updater (b1 <<< b2)
 
 instance monoidUpdater ∷ Monoid (Updater r) where
   mempty = Updater identity
 
 modify ∷ ∀ a r r' sym. IsSymbol sym ⇒ Row.Cons sym a r' r ⇒ SProxy sym → (a → a) → Updater (Record r)
-modify prop f = Updater (\r → let a = Record.get prop r in unsafeSet (reflectSymbol prop) (f a) r)
+modify prop f = Updater $ modify'
+  where
+    modify' ∷ Record.Builder.Builder (Record r) (Record r)
+    modify' = unsafeCoerce unsafeModify
+
+    unsafeModify ∷ (Record r) → (Record r)
+    unsafeModify r =
+      let a = Record.get prop r
+      in unsafeSet (reflectSymbol prop) (f a) r
 
 update ∷ ∀ r. Updater (Record r) → Record r → Record r
-update (Updater u) r = u (copyRecord r)
-
+update (Updater b) r = Record.Builder.build b r
 
 class PrefixRoutes (rl ∷ RowList) routes where
   prefixRoutes ∷ RLProxy rl → Updater { | routes }
